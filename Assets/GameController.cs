675c5b8de445be6cfd06ff74ad4b296ad3f4aa38
Niti Petcharatmora <niti.skeba11@gmail.com>
2019-01-26 23:04:07 +0700

    Commit scene with background sunset

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -13,13 +13,20 @@ public class GameController : MonoBehaviour {
 	public float currentSunHeight, maxSunHeight, minSunHeight, maxSunsetSpeed, sunsetSpeed;
 	public float collideSpeedPenalty = 2f;
 	public static GameController instance;
-	void Awake () {
+
+    public List<GameObject> buildings;
+    public List<GameObject> skys;
+    void Awake () {
 		instance = this;
 		lanesSpeed = maxLanesSpeed;
 	}
 	void Start() {
-		currentSunHeight = maxSunHeight;
-	}
+        maxSunHeight = this.sun.GetComponent<SunSystemInfo>().sun.transform.eulerAngles.z;
+        currentSunHeight = maxSunHeight;
+        buildings = this.sun.GetComponent<SunSystemInfo>().buildings;
+        skys = this.sun.GetComponent<SunSystemInfo>().skys;
+
+    }
 	void Update () {
 		if (isSunAtMin()) {
 			// Gameover
@@ -56,11 +63,60 @@ public class GameController : MonoBehaviour {
 	}
 
 	void SunSetting() {
-		Vector3 pos = sun.transform.position;
-		pos.y -= sunsetSpeed * Time.deltaTime;
-		sun.transform.position = pos;
-		currentSunHeight = pos.y;
-	}
+        GameObject sun = this.sun.GetComponent<SunSystemInfo>().sun;
+        Vector3 rot = sun.transform.eulerAngles;
+        if (rot.z > 333)
+        {
+            rot.z -= sunsetSpeed * Time.deltaTime;
+            sun.transform.eulerAngles = rot;
+            currentSunHeight = rot.z;
+
+            //Debug.Log(1 - ((maxSunHeight - currentSunHeight) / 20));
+            for (int i = 0; i < buildings.Count; i++) {
+                SpriteRenderer b = buildings[i].GetComponent<SpriteRenderer>();
+                SpriteRenderer s = skys[i].GetComponent<SpriteRenderer>();
+                Color color = b.color;
+                color.a = 1 - ((maxSunHeight - currentSunHeight) / 20);
+                b.color = color;
+                s.color = color;
+            }
+            //SpriteRenderer building = this.sun.GetComponent<SunSystemInfo>().building.GetComponent<SpriteRenderer>();
+            //Color colorB = building.color;
+            //colorB.a = 1 - ((maxSunHeight - currentSunHeight) / 20);
+            //building.color = colorB;
+
+            //SpriteRenderer sky = this.sun.GetComponent<SunSystemInfo>().sky.GetComponent<SpriteRenderer>();
+            //Color colorS = sky.color;
+            //colorS.a = 1 - ((maxSunHeight - currentSunHeight) / 20);
+            //sky.color = colorS;
+        }
+        else {
+            sun.GetComponentInChildren<Light>().intensity = 0;
+            for (int i = 0; i < buildings.Count; i++)
+            {
+                SpriteRenderer b = buildings[i].GetComponent<SpriteRenderer>();
+                SpriteRenderer s = skys[i].GetComponent<SpriteRenderer>();
+                Color color = b.color;
+                color.a = 0;
+                b.color = color;
+                s.color = color;
+            }
+            //SpriteRenderer building = this.sun.GetComponent<SunSystemInfo>().building.GetComponent<SpriteRenderer>();
+            //Color colorB = building.color;
+            //colorB.a = 0;
+            //building.color = colorB;
+
+            //SpriteRenderer sky = this.sun.GetComponent<SunSystemInfo>().sky.GetComponent<SpriteRenderer>();
+            //Color colorS = sky.color;
+            //colorS.a = 0;
+            //sky.color = colorS;
+        }
+
+        //      Vector3 pos = sun.transform.position;
+        //pos.y -= sunsetSpeed * Time.deltaTime;
+        //sun.transform.position = pos;
+        //currentSunHeight = pos.y;
+    }
 
 	bool isSunAtMin() {
 		return currentSunHeight <= minSunHeight;
---
+++ Assets/SunSystemInfo.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunSystemInfo : MonoBehaviour {
+
+    public GameObject building;
+    public GameObject sky;
+    public GameObject sun;
+    public GameObject directLight;
+    public int numberOfBgs =3;
+    public List<GameObject> skys;
+    public List<GameObject> buildings;
+    public float skysSpeed;
+    public float buildingsSpeed;
+    public GameObject skysParent;
+    public GameObject buildingsParent;
+    public GameObject detectPoint;
+    // Use this for initialization
+    void Start () {
+        GameObject bg;
+        buildings = new List<GameObject>();
+        for (int i = 0; i < numberOfBgs; i++)
+        {
+            bg = Instantiate(building);
+            bg.transform.parent = skysParent.transform;
+            Vector3 newPos = this.transform.position;
+            newPos.x += bg.GetComponent<SpriteRenderer>().bounds.size.x * i -10;
+            newPos.y = -0.7f;
+            bg.transform.position = newPos;
+            buildings.Add(bg);
+        }
+
+        skys = new List<GameObject>();
+        for (int i = 0; i < numberOfBgs; i++)
+        {
+            bg = Instantiate(sky);
+            bg.transform.parent = buildingsParent.transform;
+            Vector3 newPos = this.transform.position;
+            newPos.x += bg.GetComponent<SpriteRenderer>().bounds.size.x * i -10;
+            newPos.y = -0.5f;
+            bg.transform.position = newPos;
+            skys.Add(bg);
+        }
+
+    }
+
+    public void MoveBackground()
+    {
+        foreach (GameObject bg in skys)
+        {
+            Vector3 pos = bg.transform.position;
+            pos.x -= (skysSpeed * Time.deltaTime);
+            bg.transform.position = pos;
+        }
+
+        foreach (GameObject bg in buildings)
+        {
+            Vector3 pos = bg.transform.position;
+            pos.x -= (buildingsSpeed * Time.deltaTime);
+            bg.transform.position = pos;
+        }
+    }
+
+    public void DetectBackground(List<GameObject> list)
+    {
+        GameObject bg = list[0]; // 1st bg only collides with the detect point
+        if (isCollidedWithDetectPoint(bg))
+        {
+            // prepare a position of the last bg
+            Vector3 lastBgPos = list[numberOfBgs - 1].transform.position;
+            lastBgPos.x += bg.GetComponent<SpriteRenderer>().bounds.size.x;
+            bg.transform.position = lastBgPos;
+            SwapBackground(list);
+        }
+    }
+
+    bool isCollidedWithDetectPoint(GameObject bg)
+    {
+        return bg.transform.position.x + bg.GetComponent<SpriteRenderer>().bounds.size.x < detectPoint.transform.position.x;
+    }
+
+    void SwapBackground(List<GameObject> list)
+    {
+        GameObject firstBg = list[0];
+        list.RemoveAt(0);
+        list.Add(firstBg);
+    }
+    // Update is called once per frame
+    void Update () {
+        MoveBackground();
+        DetectBackground(skys);
+        DetectBackground(buildings);
+    }
+}
