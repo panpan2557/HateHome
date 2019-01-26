98078a039dbc87f4c07e78ffbd38678f198f4777
Punpikorn <sadanthem@gmail.com>
2019-01-27 04:31:27 +0700

    Implemented intro scene

---
+++ Assets/AnimatorHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnimatorHelper : MonoBehaviour {
+	public GameObject boyEmoji;
+	public Sprite[] boyEmojiSprites;
+	public void RandomEmoSprite() {
+		int i = Random.Range(0, boyEmojiSprites.Length);
+		boyEmoji.GetComponent<SpriteRenderer>().sprite = boyEmojiSprites[i];
+	}
+
+	public void MoveToMainScene() {
+		SceneManager.LoadScene("Main");
+	}
+}
